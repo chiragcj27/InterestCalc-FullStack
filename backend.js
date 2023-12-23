b7a0b125f5bb163f072ba462a05bc0412ec2c4e7
Chirag Jain <cj27chirag@gmail.com>
2023-12-24 01:10:32 +0530

    Done with frontend

--- backend.js
+++ backend.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+
+const cors = require('cors');
+app.use(cors());
+
 app.get("/calculate/simple-interest", function(req,res) {
     const rate = parseFloat(req.query.rate);
     const amount = parseFloat(req.query.amount);
@@ -10,7 +14,7 @@ app.get("/calculate/simple-interest", function(req,res) {
     const total = amount * (1 + (rate/100)*time);
     const interest = total - amount;
 
-    res.send({
+    res.json({
         total: total,
         interest: interest
     })
